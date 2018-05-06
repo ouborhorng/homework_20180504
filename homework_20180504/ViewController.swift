8cff45626be408571e075d9714aa76003521e357
OBO <OBO@2001-b011-5c0e-1426-18aa-9969-6721-b60d.dynamic-ip6.hinet.net>
2018-05-06 14:55:16 +0800

    V1.0

--- homework_20180504/ViewController.swift
+++ homework_20180504/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -19,7 +20,53 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBOutlet weak var number1_TextField: UITextField!
+    @IBOutlet weak var number2_TextField: UITextField!
+    @IBOutlet weak var Ans_Label: UILabel!
+    
+    @IBOutlet weak var Calculation_SegmentedControl: UISegmentedControl!
+    
+    @IBAction func Calculation_SegmentedControl(_ sender: Any) {
+        if(number1_TextField.text == "")
+        {
+            number1_TextField.text = "0"
+        }
+        if(number2_TextField.text == "")
+        {
+            number2_TextField.text = "0"
+        }
+        if(number2_TextField.text == "0" && Calculation_SegmentedControl.selectedSegmentIndex == 3)
+        {
+            number2_TextField.text = "1"
+        }
+        
+        let number1: Double? = Double(number1_TextField.text!)
+        let number2: Double? = Double(number2_TextField.text!)
+        var sum: Double? = 0
+       if(Calculation_SegmentedControl.selectedSegmentIndex == 0)
+       {
+            sum = number1! + number2!
+       }
+        else if(Calculation_SegmentedControl.selectedSegmentIndex == 1)
+       {
+            sum = number1! - number2!
+        }
+       else if(Calculation_SegmentedControl.selectedSegmentIndex == 2)
+       {
+            sum = number1! * number2!
+        }
+        else
+       {
+            sum = number1! / number2!
+        }
+        
+        Ans_Label.text = String(format: "%.1f", sum!)
+        let speedchUtterance = AVSpeechUtterance(string: Ans_Label.text!)
+        speedchUtterance.rate = 0.5
+        speedchUtterance.voice = AVSpeechSynthesisVoice(language: "fr-FR")
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(speedchUtterance)
+        
+    }
 }
 
