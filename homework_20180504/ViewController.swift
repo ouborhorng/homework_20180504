99a546d97cf24b5abed89b5d75ed9208bd89aa49
OBO <OBO@2001-b011-5c0e-1426-18aa-9969-6721-b60d.dynamic-ip6.hinet.net>
2018-05-06 11:32:18 +0800

    Initial Commit

---
+++ homework_20180504/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  homework_20180504
+//
+//  Created by OBO on 2018/5/6.
+//  Copyright © 2018年 OBO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
